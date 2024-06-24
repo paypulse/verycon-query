a9ae25614bdab6eb0f99c1ba13d8fc5c52c9e79c
minjung-shin <minjung.shin@mocah.co.kr>
2024-06-24 09:05:33 +0900

    20240613작업 내역_3 완료 버전

--- console_9.sql
+++ console_9.sql
@@ -20,6 +20,51 @@ where
 --    and BN.subject like '%이용%'
 --     and date_format(BN.createdAt,'%Y-%m-%d')  Between date_format('2024-03-01','%Y-%m-%d')  and date_format('2024-04-16','%Y-%m-%d')
 -- limit 1 offset 1 ;  -- limit perpage  offset page
+-- order by BN.uid desc
+
+
+SELECT *
+FROM BoardNotice
+         LEFT JOIN BoardCategory ON (BoardCategory.uid = BoardNotice.boardCategoryUid);
+
+
+
+SELECT BoardNotice.uid,
+       BoardNotice.boardCategoryUid,
+       BoardNotice.topOrder,
+       BoardNotice.boardType,
+       BoardNotice.subject,
+       BoardNotice.contents,
+       BoardNotice.isUse,
+       BoardNotice.isDel,
+       BoardNotice.writerSeq,
+       BoardNotice.userIp,
+       BoardNotice.updatedAt,
+       BoardNotice.createdAt,
+       BoardCategory.uid,
+       BoardCategory.boardType,
+       BoardCategory.`order`,
+       BoardCategory.`name`,
+       BoardCategory.isUse,
+       BoardCategory.isDel,
+       BoardCategory.writerSeq,
+       BoardCategory.userIP,
+       BoardCategory.updatedAt,
+       BoardCategory.createdAt
+FROM BoardNotice
+         LEFT JOIN BoardCategory ON (BoardCategory.uid = BoardNotice.boardCategoryUid)
+WHERE (BoardNotice.boardType = 'FAQ')
+  AND (BoardNotice.isUse = TRUE)
+  AND (BoardNotice.isDel = FALSE)
+  AND (BoardNotice.createdAt BETWEEN '2024-04-01' AND '2024-06-01')
+ORDER BY BoardNotice.uid DESC
+LIMIT 10;
+
+select *
+from BoardNotice;
+
+
+
 
 
 -- 2. 사용 , 미사용 상태 변경 쿼리
@@ -30,6 +75,21 @@ where
     update BoardNotice
     set isDel = 0
     where uid = 4;
+
+select *
+from BoardNotice;
+
+
+
+delete from BoardNotice
+where uid in (11,12);
+
+
+select *
+from BoardCategory;
+
+select *
+from BoardNotice;
 
 -- 3. 게시물 등록 쿼리 , default 값 :  uid=autokey, isDel =0, writerSeq = 0 ,
     insert into BoardNotice (
@@ -68,7 +128,40 @@ where
         BN.contents
     FROM BoardNotice AS BN
     LEFT JOIN BoardCategory AS BC ON BN.boardCategoryUid = BC.uid
-    WHERE BN.uid = 6;
+    WHERE BN.uid = 6 and BC.isDel = false
+    limit  1;
+
+
+SELECT BoardNotice.uid,
+       BoardNotice.boardCategoryUid,
+       BoardNotice.topOrder,
+       BoardNotice.boardType,
+       BoardNotice.subject,
+       BoardNotice.contents,
+       BoardNotice.isUse,
+       BoardNotice.isDel,
+       BoardNotice.writerSeq,
+       BoardNotice.userIp,
+       BoardNotice.updatedAt,
+       BoardNotice.createdAt,
+       BoardCategory.uid,
+       BoardCategory.boardType,
+       BoardCategory.`order`,
+       BoardCategory.`name`,
+       BoardCategory.isUse,
+       BoardCategory.isDel,
+       BoardCategory.writerSeq,
+       BoardCategory.userIP,
+       BoardCategory.updatedAt,
+       BoardCategory.createdAt
+FROM BoardNotice
+         LEFT JOIN BoardCategory ON (BoardCategory.uid = BoardNotice.boardCategoryUid)
+WHERE (BoardNotice.uid = 7)
+  AND (BoardNotice.isDel = FALSE)
+LIMIT 1;
+
+
+
 
 -- 5. 게시물 수정 쿼리
 -- 수정에 포함 되는 범주  :  상단 고정, 분류, 상태, 제목 , 내용
@@ -78,15 +171,20 @@ where
         boardCategoryUid = 1,
         isUse = 1,
         subject = '수정테스트' , -- null 일 수 없고
-        contents = '수정 수정 ' -- null 가능
+        contents = '수정 수정 ', -- null 가능
+        updatedAt = now()
     where uid = 6;
+
+
+select *
+from BoardNotice;
 
 -- 6. 분류 항목 조회 , isDel 이 0 인 애들
     select
         BC.name,
         BC.uid
     from BoardCategory as BC
-    where isDel = 0;
+    where BC.isDel = false and   boardType = 'FAQ';
 
 -- 7. 분류 추가 -- order :  분류 순번  default : 0
     insert into BoardCategory (
@@ -111,18 +209,28 @@ where
         now()  -- default
     );
 
+select *
+from BoardCategory;
+
+select *
+from BoardNotice;
+
+
+
 -- 다건 수정 / 삭제
 -- 삭제 /수정 완료시 success : true 로 올 수 잇게 해주기  저장을 누르는 순간 해당 내역 반영
 -- 8.분류 수정 쿼리  --> 카테 고리 분류 편집 쿼리
     update BoardCategory
         set name ='분류121'
-    where uid = 7;
+    where uid = 7 and boardType = 'FAQ';
 
 -- 9. 분류 삭제 --> 다건에 해당
     update BoardCategory
         set isDel = 1
-    where uid = 7;
+    where uid = 7 and boardType = 'FAQ';
 --
+select *
+from BoardCategory;
 
 -- 문의하기
 select *
@@ -145,19 +253,135 @@ select
     BU.subject,
     BU.createdAt
 from BoardContactUs as BU
-join User as U on BU.seqUser = U.seq
+inner join User as U on BU.seqUser = U.seq
 where
-    BU.boardType = 'BULK_SEND'
-    and BU.isUse = 1
-    and BU.depth = 0 -- 답변이면 1에 해당
-    and U.userId like '%ki%'
-    and date_format(BU.createdAt,'%Y-%m-%d')  Between date_format('2024-04-01','%Y-%m-%d')  and date_format('2024-06-16','%Y-%m-%d')
-    limit 1 offset 1 ;  -- limit perpage  offset page
+    BU.boardType = 'BULK_SEND';
+  --  and BU.isUse = 1
+  --  and BU.depth = 0 -- 답변이면 1에 해당
+  --  and U.userId like '%ki%'
+  --  and date_format(BU.createdAt,'%Y-%m-%d')  Between date_format('2024-04-01','%Y-%m-%d')  and date_format('2024-06-16','%Y-%m-%d')
+  --  limit 1 offset 1 ;  -- limit perpage  offset page
+
+
+-- 2024052215443011200000002
+
+      select *
+      from User
+      where seq = '2024052215443011200000002';
+
+
+
+ SELECT COUNT(*)
+ FROM BoardContactUs INNER JOIN `User` ON  (BoardContactUs.seqUser = `User`.seq)
+ WHERE (BoardContactUs.isUse = TRUE)
+   AND (BoardContactUs.`depth` = 0)
+  -- AND (BoardContactUs.boardType = 'API')
+  -- AND (`User`.userId LIKE '%gonna%')
+  -- AND (BoardContactUs.createdAt BETWEEN 2024-04-17 AND 2024-06-17) ;
+
+
+
+
+select *
+from User
+where userId = '%ki';
 
 -- 11. 문의 구분 조회
 select
     DISTINCT boardType
 from BoardContactUs;
+
+select *
+from BoardContactUs;
+
+ SELECT COUNT(*)
+ FROM BoardContactUs
+     INNER JOIN `User` ON  (BoardContactUs.seqUser = `User`.seq)
+ WHERE
+     (BoardContactUs.isUse = 1)
+    AND (BoardContactUs.`depth` = 0)
+    AND (BoardContactUs.boardType = '')
+    AND (`User`.userId LIKE '%gonna%');
+  -- AND (BoardContactUs.createdAt BETWEEN 2024-01-01 AND 2024-06-01);
+ SELECT *
+ FROM BoardContactUs
+     INNER JOIN `User` ON  (BoardContactUs.seqUser = `User`.seq);
+
+
+
+SELECT
+    BoardContactUs.uid,
+    BoardContactUs.ownerUid,
+    BoardContactUs.`depth`,
+    BoardContactUs.boardType,
+    BoardContactUs.seqUser,
+    BoardContactUs.`name`,
+    BoardContactUs.email,
+    BoardContactUs.phone,
+    BoardContactUs.subject,
+    BoardContactUs.contents,
+    BoardContactUs.isAnswer,
+    BoardContactUs.isUse,
+    BoardContactUs.isDel,
+    BoardContactUs.userIP,
+    BoardContactUs.updatedAt,
+    BoardContactUs.createdAt,
+    `User`.seq,
+    `User`.companySeq,
+    `User`.userId,
+    `User`.password,
+    `User`.`name`,
+    `User`.email,
+    `User`.cellPhone,
+    `User`.loginFailCount,
+    `User`.lastPasswordUpdatedAt,
+    `User`.adServiceAgreeAt,
+    `User`.isUse,
+    `User`.userIP,
+    `User`.ci,
+    `User`.di,
+    `User`.lastLoginToken,
+    `User`.updatedAt,
+    `User`.createdAt
+FROM BoardContactUs INNER JOIN `User` ON  (BoardContactUs.seqUser = `User`.seq)
+WHERE (BoardContactUs.isUse = TRUE)
+  AND (BoardContactUs.`depth` = 0)
+  AND (BoardContactUs.boardType = 'API')
+  AND (`User`.userId LIKE '%k%')
+  AND (BoardContactUs.createdAt BETWEEN 2024-01-01 AND 2024-06-01)
+ORDER BY BoardContactUs.uid DESC LIMIT 10;
+
+
+
+select *
+from BoardContactUs;
+
+SELECT
+       BoardContactUs.isUse,
+       BoardContactUs.depth,
+       User.userId,
+       boardType,
+       BoardContactUs.createdAt
+FROM BoardContactUs
+         INNER JOIN `User` ON (BoardContactUs.seqUser = `User`.seq);
+
+SELECT
+    BoardContactUs.isUse,
+       BoardContactUs.depth,
+       User.userId,
+       boardType,
+       BoardContactUs.createdAt
+FROM BoardContactUs
+         INNER JOIN `User` ON (BoardContactUs.seqUser = `User`.seq)
+ WHERE
+ --   (BoardContactUs.isUse = TRUE)
+ --    AND BoardContactUs.isDel = false
+ -- AND (BoardContactUs.`depth` = 0)
+   (BoardContactUs.createdAt BETWEEN '2023-01-01' AND '2024-06-01')
+ORDER BY BoardContactUs.uid DESC
+LIMIT 100;
+
+
 
 -- 12. 문의 내역 상세 조회
 -- 답글은 그냥 조인 하지 말고 노출 해야 되는 문제
@@ -176,6 +400,15 @@ from BoardContactUs as BU1
 inner join BoardContactUs as BU2 on BU1.uid = BU2.ownerUid
 where BU1.seqUser = '2024060516125675900000029';
 
+select *
+from Category;
+
+select *
+from BoardContactUs;
+
+select *
+from BoardCategory;
+
 
 -- 13. 문의 내역 답변 수정
 -- 이렇게 join  걸지 말기 !!!
@@ -184,6 +417,14 @@ join BoardContactUs as BU2 on BU1.uid = BU2.ownerUid
 set BU2.subject = '답변 수정 test' ,
     BU2.contents = '답변 수정 contents '
 where BU1.uid = 13;
+
+update BoardContactUs
+set isAnswer = true
+where uid  = 13
+
+
+select *
+from BoardContactUs;
 
 -- 14. 문의 내역 답변 등록
 INSERT INTO BoardContactUs
@@ -217,6 +458,16 @@ INSERT INTO BoardContactUs
     );
 
 
+delete from BoardContactUs
+where uid in (21);
+
+
+
+
+
+select *
+from BoardContactUs;
+
 --
 -- 초기 AdminUser에 대한 Password 등록
 select *
@@ -234,6 +485,74 @@ values ( '2024052215443011200000002','2024052215443011200000002', '9e4294f51f9fe
        ( '2024052215443011200000001','2024052215443011200000001', 'a69343ac7ef586435f708c4fc0a0d5d84386d5f5cc5663004169452c8d1cf057', '127.0.0.1',NOW(),NOW());
 
 
+
+--
+SELECT
+    BoardNotice.uid,
+    BoardNotice.boardCategoryUid,
+    BoardNotice.topOrder,
+    BoardNotice.boardType,
+    BoardNotice.subject,
+    BoardNotice.contents,
+    BoardNotice.isUse,
+    BoardNotice.isDel,
+    BoardNotice.updatedAt,
+    BoardNotice.createdAt,
+    BoardCategory.uid,
+    BoardCategory.boardType,
+    BoardCategory.`order`,
+    BoardCategory.`name`,
+    BoardCategory.isUse,
+    BoardCategory.isDel,
+    BoardCategory.createdAt
+FROM BoardNotice
+    LEFT JOIN BoardCategory ON  (BoardCategory.uid = BoardNotice.boardCategoryUid)
+WHERE (BoardNotice.boardType = 'FAQ')
+  AND (BoardNotice.isUse = true)
+  AND (BoardNotice.isDel = false)
+  AND (BoardNotice.boardCategoryUid = 2)
+ORDER BY BoardNotice.uid DESC LIMIT 10;
+
+
+select *
+from BoardCategory
+where boardType = 'FAQ';
+
+
+select *
+from BoardNotice;
+
+
+select uid
+from BoardContactUs;
+
+delete from BoardNotice
+where uid in (15,16,17);
+
+
+select *
+from BoardCategory;
+
+delete from BoardCategory
+where uid = 13;
+
+
+select *
+from GroupItem;
+
+select *
+from GroupItemGoods;
+
+
+select *
+from GroupTheme;
+
+select *
+from GroupThemeBrand;
+
+
+select *
+from GroupThemeBrandGoods;
 
 
 
